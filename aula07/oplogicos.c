a43d5b8366fec1af6c6c015e8698d652922f8926
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-02-02 06:42:06 -0300

    Operadores Lógicos

---
+++ aula07/oplogicos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ *  Conjunção (E, AND) = &&
+ *  Disjunção (OU, OR) = ||
+*/
+
+
+
+int main(void){
+
+        int a = 8;
+        int b = 9;
+
+        printf("%d\n", a == b || b > 4);
+
+        return 1;
+}
