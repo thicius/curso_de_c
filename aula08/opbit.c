303b585369f566933d835ea0d50add745bb69cb1
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-02-05 09:39:33 -0300

    Aula 08 - bit-a-bit

---
+++ aula08/opbit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+        int a = 2; // 10
+        int b = 3; // 11
+
+// & (e)
+// | (ou)
+// ^ xor (ou exclusivo)
+// ~ (negação)
+// << (shift left)
+// >> (shift right)
+
+
+        // 0 falso
+        // qualquer coisa diferente de 0 eh verdadeiro
+        
+         printf("o resultado eh %d\n", a & b);
+         // teste os outros operadores no lugar de &
+
+return 0;
+}
+
+// o sinal << é como multiplicar um número pela base decimal
+// " 123 << 2" vai ser "12300"
